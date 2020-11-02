f5977a7e98b863abc787fd243a5961cbd034b32b
Vladimir Mednis <mednisv@yandex.ru>
2020-11-02 23:49:18 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 25000;
+        int bonus = TicketPrice / 20;
+        System.out.println(bonus);
+    }
+}
